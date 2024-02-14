62f7d1819c416d80de4dda8e4b23f3a16b5d2358
root <root@liulilte>
2024-02-14 10:56:45 +0800

    Add Files.

---
+++ armv7l/boost/libs/context/CMakeFiles/boost_context.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/root/dev_armv7l/boost/libs/context/src/asm/jump_x86_64_sysv_elf_gas.S" "/root/dev_armv7l/boost/libs/context/CMakeFiles/boost_context.dir/src/asm/jump_x86_64_sysv_elf_gas.S.o"
+  "/root/dev_armv7l/boost/libs/context/src/asm/make_x86_64_sysv_elf_gas.S" "/root/dev_armv7l/boost/libs/context/CMakeFiles/boost_context.dir/src/asm/make_x86_64_sysv_elf_gas.S.o"
+  "/root/dev_armv7l/boost/libs/context/src/asm/ontop_x86_64_sysv_elf_gas.S" "/root/dev_armv7l/boost/libs/context/CMakeFiles/boost_context.dir/src/asm/ontop_x86_64_sysv_elf_gas.S.o"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "BOOST_CONTEXT_EXPORT="
+  "BOOST_CONTEXT_NO_LIB="
+  "BOOST_CONTEXT_SOURCE="
+  "BOOST_CONTEXT_STATIC_LINK="
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/dev_armv7l/boost/libs/context/src/posix/stack_traits.cpp" "/root/dev_armv7l/boost/libs/context/CMakeFiles/boost_context.dir/src/posix/stack_traits.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_CONTEXT_EXPORT="
+  "BOOST_CONTEXT_NO_LIB="
+  "BOOST_CONTEXT_SOURCE="
+  "BOOST_CONTEXT_STATIC_LINK="
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ armv7l/boost/libs/context/CMakeFiles/boost_context.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/boost_context.dir/src/asm/make_x86_64_sysv_elf_gas.S.o"
+  "CMakeFiles/boost_context.dir/src/asm/jump_x86_64_sysv_elf_gas.S.o"
+  "CMakeFiles/boost_context.dir/src/asm/ontop_x86_64_sysv_elf_gas.S.o"
+  "CMakeFiles/boost_context.dir/src/posix/stack_traits.cpp.o"
+  "libboost_context.pdb"
+  "libboost_context.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM CXX)
+  include(CMakeFiles/boost_context.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
